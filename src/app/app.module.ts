ea8bfe77c923b16ebf011719580bac06ea4f80dc
ines uwase <uwaseines10@gmail.com>
2019-08-29 12:45:05 +0200

    added elementsin constructor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubComponent
+    GithubComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/strikethrough.directive.ts
@@ -0,0 +1,12 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appStrikethrough]'
+})
+export class StrikethroughDirective {
+
+  constructor() {
+    this.elem.nativeElement.style.textDecoration='line-through';
+   }
+
+}
