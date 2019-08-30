9af36e24db457031c6cd234ab8f4b83194cb0bf6
ines uwase <uwaseines10@gmail.com>
2019-08-30 09:41:29 +0200

    added a service

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+   user: User;
+  constructor(private githubRequestService: GithubRequestService) {
+    this.githubRequestService = githubRequestService;
+   }
 
   ngOnInit() {
+    this.githubRequestService.githubRequest()
+    this.user = this.githubRequestService.user
   }
 
 }
